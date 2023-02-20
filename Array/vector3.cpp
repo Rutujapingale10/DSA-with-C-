2abfbe1cc3d14dab7f3428a03f48b06259867417
Rutuja Pingale <67416437+Rutujapingale10@users.noreply.github.com>
2023-02-20 13:04:35 +0530

    intersection of 2 array

--- Array/vector3.cpp
+++ Array/vector3.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-	vector<int>array{1,2,3,4,5,6,8};
+	vector<int>array{1,2,3,3,4,5,6,8};
 	vector<int>array1{10,11,2,3,9};
 	vector<int>intersection;
 	
@@ -16,6 +16,7 @@ int main()
 		{
 			if(element==array1[j])
 			{
+				array1[j]=-1;    //to handel duplicate
 				intersection.push_back(element);
 			}
 		}
